a38ffb0d5b3b5541f1d3fb2dccd8920a31403395
akrmyuc <87920616+akrmyuc@users.noreply.github.com>
2022-05-04 17:30:58 +0900

    heroes コンポーネントを作成

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// 以下は ng generate component heroes で自動的に追加された
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // 以下は ng generate component heroes で自動的に追加された
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
