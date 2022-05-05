78e98842b263eab4c62c07d779196c23855ecde9
akrmyuc <87920616+akrmyuc@users.noreply.github.com>
2022-05-05 22:49:12 +0900

    FormsModule をインポート

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-// 以下は ng generate component heroes で自動的に追加された
 import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    // 以下は ng generate component heroes で自動的に追加された
     HeroesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
